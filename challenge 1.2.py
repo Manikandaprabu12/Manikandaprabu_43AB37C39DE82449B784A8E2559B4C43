4e5c953ab01810ad97f7b52dc0a29be541d1afb6
Manikandaprabu12 <145438494+Manikandaprabu12@users.noreply.github.com>
2023-09-19 17:11:32 +0530

    Add files via upload

--- challenge 1.2.py
+++ challenge 1.2.py
@@ -1,7 +1,9 @@
-year = int(input("Enter a year: "))
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
-else:
-    print("{0} is not a leap year".format(year))
+def factorial(n):
+      if (n==1 or n==0):
+           return 1
+      else:
+           return (n * factorial(n - 1))
+
+num = 5;
+print("number : ",num)
+print("Factorial : ",factorial(num))
